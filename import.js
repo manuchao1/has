ec965d82da7af73410e35c6fa77efe5aa109a29d
Manu Chao <m.chao@gmx.de>
2016-02-13 16:05:54 +0100

    Implement importer

--- import.js
+++ import.js
@@ -1,8 +1,8 @@
 create_storage().then(function(storage) {
 	$(document).ready(function() {
 		$('#start').click(function() {
-			var customers = $('texarea#customers').val();
-			var items = $('texarea#items').val();
+			var customers = $('textarea#customers').val();
+			var items = $('textarea#items').val();
 
 			console.log(customers);
 			console.log(items);
@@ -12,6 +12,28 @@ create_storage().then(function(storage) {
 
 			console.log(customers);
 			console.log(items);
+			var promises = customers.map(function(c) {
+				var gender;
+				if (c.anrede == 'Frau') {
+					gender = 'female';
+				} else if (c.anrede == 'Herr') {
+					gender = 'male';
+				} else {
+					gender = 'child';
+				}
+				var customer = new Customer(c.id, c.vorname, c.nachname, gender, c.strasse, c.plz, c.ort);
+				console.log('creating customer', c.id);
+				return storage.create_customer(customer);
+			});
+			Promise.all(promises).then(function() {
+				var promises = items.map(function(i) {
+					if (i.versteckt == 0) {
+						var item = new Item(i.id, i.ziffer, i.beschreibung, i.preis);
+						console.log('creating item', i.id);
+						return storage.create_item(item);
+					}
+				});
+			})
 		})
 	});
 });
