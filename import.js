ed5706c7113a634babfa95d507510b2888d07625
Manu Chao <m.chao@gmx.de>
2016-02-13 18:26:40 +0100

    Do not do it for bills

--- import.js
+++ import.js
@@ -48,9 +48,9 @@ create_storage().then(function(storage) {
 								price: (i.preis*100)|0,
 							};
 						});
-						var bill = new Bill(b.id, b.patient, b.diagnose, new Date(b.rechnungsdatum), '', false, false, positions);
+						var bill = new Bill(undefined, b.patient, b.diagnose, new Date(b.rechnungsdatum), '', false, false, positions);
 						console.log(bill);
-						storage.save_bill(bill);
+						storage.create_bill(bill);
 					});
 				});
 			})
